f34c07832fd3c582aa8fc4fa53dd952c872a09c3
Vlad <shalamay99@gmail.com>
2026-08-15 11:17:04 +0300

    feat: setup feedback section

---
+++ src/js/axios.js
@@ -0,0 +1,13 @@
+async function getFeedbacks() {
+  const response = await axios.get(
+    'https://deserts-store.b.goit.study/api/feedbacks',
+    {
+      params: {
+        limit: 10,
+        page: 1,
+      },
+    }
+  );
+
+  return response.data;
+}
