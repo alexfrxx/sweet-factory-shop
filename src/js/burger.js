fc88ccbf349f913e1e23d9cf1b7cd1031cb87f27
Alex Frolov <fr.280907@gmail.com>
2026-08-18 14:09:14 +0200

    fixes

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,21 +1,28 @@
+const burger = document.querySelector('.mobile-menu');
 const burgerBtn = document.querySelector('.burger-btn');
 const closeBtn = document.querySelector('.close-btn');
-const mobileMenu = document.querySelector('.mobile-menu');
-const mobileMenuLinks = mobileMenu.querySelectorAll('a');
-
-function openMenu() {
-  mobileMenu.classList.add('is-open');
-  burgerBtn.setAttribute('aria-expanded', 'true');
-}
-
-function closeMenu() {
-  mobileMenu.classList.remove('is-open');
-  burgerBtn.setAttribute('aria-expanded', 'false');
-}
 
 burgerBtn.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
 
-mobileMenuLinks.forEach(link => {
-  link.addEventListener('click', closeMenu);
+burger.addEventListener('click', e => {
+  if (e.target.classList.contains('header-link')) closeMenu();
 });
+
+document.addEventListener('keydown', onEscapeClick);
+
+function onEscapeClick(e) {
+  if (e.key === 'Escape') {
+    closeMenu();
+  }
+}
+
+function openMenu() {
+  burger.classList.add('is-open');
+  burgerBtn.setAttribute('aria-expanded', 'true');
+}
+
+function closeMenu() {
+  burger.classList.remove('is-open');
+  burgerBtn.setAttribute('aria-expanded', 'false');
+}
